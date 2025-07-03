eb133d679f4e803bcc159d496661408f3341f2a8
Sabith <masterdevsabith@gmail.com>
2025-07-03 21:26:54 +0530

    ui designed

--- frontend/xploitagent/app/page.tsx
+++ frontend/xploitagent/app/page.tsx
@@ -1,11 +1,17 @@
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/includes/Navbar";
+import PainPoint from "./components/PainPoint";
+import Solution from "./components/Solution";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <PainPoint />
+      <Solution />
+      <Features />
     </main>
   );
 }
