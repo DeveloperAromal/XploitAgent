04cbe2147a4602a5b0c45b80f5ce513813cb2043
Aromal <aromals161@gmail.com>
2025-07-07 06:56:58 +0530

    updated hero

--- frontend/xploitagent/app/components/includes/Navbar.tsx
+++ frontend/xploitagent/app/components/includes/Navbar.tsx
@@ -6,37 +6,48 @@ export default function Navbar() {
     <header className="fixed w-full py-6 bg-black z-20">
       <div className="absolute inset-x-0 top-full h-12 bg-linear-to-b from-black to-transparent md:h-14 lg:h-18 z-10" />
       <div>
-        <nav className="flex justify-between items-center px-10">
-          <div className="flex items-center justify-center gap-[3rem]">
-            <Link
-              href="#"
-              className="text-cyan-400 flex items-center justify-center"
-            >
-              <h1>
-                <Image
-                  src="/assets/logo.png"
-                  alt="logo"
-                  width={40}
-                  height={240}
-                />
-              </h1>
-            </Link>
-          </div>
-          <div>
-            <ul className="flex gap-[3rem] h-auto justify-">
-              <Link href="#" className="hover:text-cyan-400">
-                <li>Features</li>
+        <nav className="px-10 flex justify-between">
+          <div className="flex items-center gap-10">
+            <div className="flex items-center justify-center">
+              <Link href="#"className="relative mt-1">
+                <h1>
+                  <Image
+                    src="/assets/logo.png"
+                    alt="logo"
+                    width={40}
+                    height={240}
+                  />
+                </h1>
               </Link>
-              <Link href="#" className="hover:text-cyan-400">
-                <li>Resources</li>
-              </Link>
-              <Link href="#" className="hover:text-cyan-400">
-                <li>Pricing</li>
-              </Link>
-              <Link href="#" className="hover:text-cyan-400">
-                <li>Enterprise</li>
-              </Link>
-            </ul>
+            </div>
+            <div>
+              <ul className="flex gap-[3rem] h-auto">
+                <Link
+                  href="#"
+                  className="hover:bg-zinc-200/5 rounded-md px-4 py-2 text-white"
+                >
+                  <li>Features</li>
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:bg-zinc-200/5 rounded-md px-4 py-2 text-white"
+                >
+                  <li>Resources</li>
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:bg-zinc-200/5 rounded-md px-4 py-2 text-white"
+                >
+                  <li>Pricing</li>
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:bg-zinc-200/5 rounded-md px-4 py-2 text-white"
+                >
+                  <li>Enterprise</li>
+                </Link>
+              </ul>
+            </div>
           </div>
 
           <div>
